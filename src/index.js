ebb61ca3640972fe8282f0b079c0fa896acfd4ee
Pedro Magalhaes <pmagalhaes@dminc.com>
2018-03-30 16:22:28 +0700

    Semantic UI and templates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { AppContainer } from 'react-hot-loader'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+render(App)
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', () => { render(App) })
+}
