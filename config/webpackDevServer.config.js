79e88e67aabeb34a0fa6887639b33ea56e3f07ae
Pedro Magalhaes <pmagalhaes@dminc.com>
2018-04-02 15:06:05 +0700

    Add concur task

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -86,7 +86,7 @@ module.exports = function (proxy, allowedHost) {
     public: allowedHost,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000'
+        target: 'http://localhost:3002'
       }
     },
     before(app) {
