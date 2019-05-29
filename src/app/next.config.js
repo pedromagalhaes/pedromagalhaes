afcdf76923dc57a3a41843875cc3d744a698048f
Pedro Magalhaes <pedro.magalhaes@outlook.com>
2019-05-29 10:37:34 +0700

    Remove webpack bundle analyzer

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -10,29 +10,7 @@ const runtimeId = nanoid()
 
 const nextPlugins = []
 
-let analyzeTarget = ''
-if (process.argv.indexOf('--bundle=server') !== -1) analyzeTarget = 'server'
-else if (process.argv.indexOf('--bundle=client') !== -1) analyzeTarget = 'client'
-else if (process.argv.indexOf('--bundle=both') !== -1) analyzeTarget = 'both'
-
 nextPlugins.push(require('@zeit/next-source-maps'))
-nextPlugins.push([
-  require('@zeit/next-bundle-analyzer'),
-  {
-    analyzeServer: analyzeTarget === 'server' || analyzeTarget === 'both',
-    analyzeBrowser: analyzeTarget === 'client' || analyzeTarget === 'both',
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html'
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html'
-      }
-    }
-  }
-])
 
 const nextConfig = {
   transpileModules: ['theme'],
@@ -119,50 +97,7 @@ const nextConfig = {
     )
 
     return config
-  }/* ,
-  exportPathMap() {
-    return {
-      // public
-      '/about': { page: '/public/about/' },
-      '/projects': { page: '/public/projects' },
-      '/services': { page: '/public/services' },
-      '/blog': { page: '/public/blog' },
-      '/contact': { page: '/public/contact' },
-      '/privacy': { page: '/public/legal/privacy' },
-      '/terms': { page: '/public/legal/terms' },
-      '/templates/colors': { page: '/public/templates/colors' },
-      '/confirmation': { page: '/public/authentication/confirmation' },
-      '/changepassword': { page: '/public/authentication/changepassword' },
-      '/forgotpassword': { page: '/public/authentication/forgotpassword' },
-      '/login': { page: '/public/authentication/login' },
-      '/register': { page: '/public/authentication/register' },
-      '/search': { page: '/public/search' },
-      // user
-      '/dashboard': { page: '/dashboard' },
-      '/dashboard/profile': { page: '/dashboard/profile' },
-      // admin
-      '/admin': { page: '/admin/login' },
-      '/admin/menu': { page: '/admin/menu' },
-      '/admin/tools': { page: '/admin/tools/modules' },
-      '/admin/tools/labels': { page: '/admin/tools/labels' },
-      '/admin/pages': { page: '/admin/pages/home' },
-      '/admin/pages/about': { page: '/admin/pages/about' },
-      '/admin/pages/changepassword': { page: '/admin/pages/changepassword' },
-      '/admin/pages/contact': { page: '/admin/pages/contact' },
-      '/admin/pages/forgotpassword': { page: '/admin/pages/forgotpassword' },
-      '/admin/pages/login': { page: '/admin/pages/login' },
-      '/admin/pages/register': { page: '/admin/pages/register' },
-      '/admin/pages/privacy': { page: '/admin/pages/privacy' },
-      '/admin/pages/terms': { page: '/admin/pages/terms' },
-      '/admin/pages/projects': { page: '/admin/pages/projects' },
-      '/admin/pages/dashboard': { page: '/admin/pages/dashboard' },
-      '/admin/pages/dashboard/profile': { page: '/admin/pages/dashboard/profile' },
-      '/admin/pages/blog': { page: '/admin/pages/blog' },
-      '/admin/pages/blog/category': { page: '/admin/pages/blog/category' },
-      '/admin/pages/blog/post': { page: '/admin/pages/blog/post' },
-      '/admin/pages/blog/posts': { page: '/admin/pages/blog/posts' }
-    }
-  } */
+  }
 }
 
 module.exports = withPlugins(nextPlugins, withTM(withCSS(nextConfig)))
